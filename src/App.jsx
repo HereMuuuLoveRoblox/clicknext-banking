f9c8de89c3663d001dbad2ffdde0afd8cc3754c6
HereMuuuLoveRoblox <misterjittakorn@gmail.com>
2026-08-22 10:40:05 +0700

    create base structure project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-primary">ClickNext Banking</h1>
+    </div>
+  )
+}
+
+export default App
